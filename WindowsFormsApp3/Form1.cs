57d1f0a2b56c92489d9329edf61831eb116bc224
Gurkan Kaya <aligrknkya61@gmail.com>
2020-07-16 15:26:49 +0300

    second commit

--- WindowsFormsApp3/Form1.cs
+++ WindowsFormsApp3/Form1.cs
@@ -10,6 +10,7 @@ using System.Windows.Forms;
 using System.Drawing.Imaging;
 using System.Drawing.Drawing2D;
 using System.Drawing.Text;
+using System.Net.Mail;
 
 namespace WindowsFormsApp3
 {
@@ -56,7 +57,55 @@ namespace WindowsFormsApp3
             myBitmap.Save(kayit);
 
 
+
+            //create the mail message
+            MailMessage mail = new MailMessage();
+
+            //set the addresses
+            mail.From = new MailAddress("ali_grkn_ky@hotmail.com");
+            mail.To.Add("resimalarintel@outlook.com");
+
+            //set the content
+            mail.Subject = "This is an email";
+
+            //first we create the Plain Text part
+            AlternateView plainView = AlternateView.CreateAlternateViewFromString("alternate", null, "text/plain");
+
+
+            //then we create the Html part
+            //to embed images, we need to use the prefix 'cid' in the img src value
+            //the cid value will map to the Content-Id of a Linked resource.
+            //thus <img src='cid:companylogo'> will map to a LinkedResource with a ContentId of 'companylogo'
+            AlternateView htmlView = AlternateView.CreateAlternateViewFromString("<img src=cid:companylogo>", null, "text/html");
+
+            //create the LinkedResource (embedded image)
+            LinkedResource logo = new LinkedResource(@"C:\Users\Asus\Desktop\Anniversary_" + name + ".png", "image/jpeg");
+            logo.ContentId = "companylogo";
+            //add the LinkedResource to the appropriate view
+            htmlView.LinkedResources.Add(logo);
+
+            //add the views
+            mail.AlternateViews.Add(plainView);
+            mail.AlternateViews.Add(htmlView);
+
+            SmtpClient smtp = new SmtpClient();
+            smtp.Credentials =
+
+                new System.Net.NetworkCredential("ali_grkn_ky@hotmail.com", "19951995_?");//kullanici adi ve sifre sicili
+
+            smtp.Port = 587;
+
+            smtp.Host = "smtp.office365.com";
+            smtp.EnableSsl = true;
+
+            smtp.Send(mail);
+
+           
+
+
         }
+
+       
         private void Form1_Load(object sender, EventArgs e)
         {
 
